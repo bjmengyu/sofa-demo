8092c89e08d1ae2173187eef50a3d8b76455824b
yu115 <yu115@163.com>
2019-01-23 21:20:12 +0800

    提交测试

--- sofa-rpc/src/main/java/com/mengyu/sofarpc/impl/MessageServiceImpl.java
+++ sofa-rpc/src/main/java/com/mengyu/sofarpc/impl/MessageServiceImpl.java
@@ -1,13 +1,14 @@
 package com.mengyu.sofarpc.impl;
 
 import com.alipay.sofa.runtime.api.annotation.SofaService;
+import com.alipay.sofa.runtime.api.annotation.SofaServiceBinding;
 import com.mengyu.sofaapi.MessageService;
 
 /**
  * @author 见善
  * @date 2019/1/16 下午7:47
  */
-@SofaService(interfaceType=MessageService.class,uniqueId = "messageService")
+@SofaService(interfaceType=MessageService.class,uniqueId = "messageService",bindings = { @SofaServiceBinding(bindingType = "bolt")})
 public class MessageServiceImpl implements MessageService {
     @Override
     public String message(String message) {
--- sofa-web/src/main/java/com/mengyu/sofaweb/controller/TestController.java
+++ sofa-web/src/main/java/com/mengyu/sofaweb/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.mengyu.sofaweb.controller;
 
 import com.alipay.sofa.runtime.api.annotation.SofaReference;
+import com.alipay.sofa.runtime.api.annotation.SofaReferenceBinding;
 import com.mengyu.sofaapi.MessageService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "test")
 public class TestController {
 
-    @SofaReference(uniqueId = "messageService")
+    @SofaReference(interfaceType = MessageService.class, binding = @SofaReferenceBinding(bindingType = "bolt"))
     private MessageService messageService;
 
     @RequestMapping("/test")
